a72adb5c3d63aa17cec6b912a13e60a2fab0b671
WerbystonLemos <werbyston@hotmail.com>
2026-05-04 00:40:14 -0300

    [Feature] add regra de negócio para verificar user e pass do formulário de login de mesma tela

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -34,6 +34,12 @@
                                 <button type="submit" class="btn btn-secondary w-100">LOGIN</button>
                             </div>
                         </form>
+
+                        @if(session('loginError'))
+                            <div class="alert alert-danger text-center">
+                                {{ session('loginError') }}
+                            </div>
+                        @endif
                     </div>
                 </div>
 
