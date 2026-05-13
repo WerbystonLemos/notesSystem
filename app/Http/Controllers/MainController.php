a5bfe32c712360a339870c624c3eef2927e1b230
WerbystonLemos <werbyston@hotmail.com>
2026-05-12 21:04:34 -0300

    [Feature] criação das rotas de delete e edit, ajuste da view de notas e criação dos métodos de edit e delete de controller de notas

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Contracts\Encryption\DecryptException;
 
 class MainController extends Controller
 {
@@ -21,6 +23,32 @@ class MainController extends Controller
     public function newNote()
     {
         echo "I'm create a new note";
+    }
+
+    public function editNote($id)
+    {
+        $id = $this->decryptId($id);
+        echo "I'm editing note with id = $id";
+    }
+        
+    public function deleteNote($id)
+    {
+        $id = $this->decryptId($id);
+        echo "I'm deleting note with id = $id";
+    }
+
+    private function decryptId($id)
+    {
+        try
+        {
+            $id = Crypt::decrypt($id);
+        }
+        catch( DecryptException $e)
+        {
+            return redirect()->route('home');
+        }
+
+        return $id;
     }
         
 }
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,8 @@ Route::middleware([CheckIsNotLogged::class])->group(function(){
 Route::middleware([CheckIsLogged::class])->group(function() {
     Route::get('/', [MainController::class, 'index'])->name('home');
     Route::get('/newNote', [MainController::class, 'newNote'])->name('new');
+    Route::get('/edit/{id}', [MainController::class, 'editNote'])->name('edit');
+    Route::get('/delete/{id}', [MainController::class, 'deleteNote'])->name('delete');
+    
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
