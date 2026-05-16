6bef28c97676fe33c8b90da0e3fe2220b223a776
WerbystonLemos <werbyston@hotmail.com>
2026-05-16 13:50:36 -0300

    [Feature] add funcoes de delete e edit note bem como acrescentado service de Operações

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Services\Operations;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Crypt;
-use Illuminate\Contracts\Encryption\DecryptException;
+// use Illuminate\Support\Facades\Crypt;
+// use Illuminate\Contracts\Encryption\DecryptException;
 
 class MainController extends Controller
 {
@@ -27,28 +28,16 @@ class MainController extends Controller
 
     public function editNote($id)
     {
-        $id = $this->decryptId($id);
+        // $id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
         echo "I'm editing note with id = $id";
     }
         
     public function deleteNote($id)
     {
-        $id = $this->decryptId($id);
+        // $id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
         echo "I'm deleting note with id = $id";
-    }
-
-    private function decryptId($id)
-    {
-        try
-        {
-            $id = Crypt::decrypt($id);
-        }
-        catch( DecryptException $e)
-        {
-            return redirect()->route('home');
-        }
-
-        return $id;
     }
         
 }
